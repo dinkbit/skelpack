4f55bfcc06b30ea12851886e91cc30027791d55a
Joseph Cohen <joseph.cohen@dinkbit.com>
2015-02-26 21:13:05 -0600

    First layout

---
+++ src/SkelPack.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ * This file is part of SkelPack.
+ *
+ * (c) Joseph Cohen <joseph.cohen@dinkbit.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Dinkbit\SkelPack\SkelPack;
+
+/**
+ * This is the skelpack class.
+ *
+ * @author Joseph Cohen <joseph.cohen@dinkbit.com>
+ */
+class SkelPack
+{	
+}
---
+++ tests/SkelPackTest.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ * This file is part of SkelPack.
+ *
+ * (c) Joseph Cohen <joseph.cohen@dinkbit.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Dinkbit\SkelPack\Tests;
+
+/**
+ * This is the skelpack class.
+ *
+ * @author Joseph Cohen <joseph.cohen@dinkbit.com>
+ */
+class SkelPackTest extends PHPUnit_Framework_TestCase
+{
+}
