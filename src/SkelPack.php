6d4d9898d23c51525f4e62b23799984400273513
Joseph Cohen <joseph.cohen@dinkbit.com>
2015-02-28 12:32:42 -0600

    Apply Style CI fixes

--- src/SkelPack.php
+++ src/SkelPack.php
@@ -8,6 +8,7 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
+
 namespace Dinkbit\SkelPack\SkelPack;
 
 /**
@@ -16,5 +17,5 @@ namespace Dinkbit\SkelPack\SkelPack;
  * @author Joseph Cohen <joseph.cohen@dinkbit.com>
  */
 class SkelPack
-{	
+{
 }
--- tests/SkelPackTest.php
+++ tests/SkelPackTest.php
@@ -8,7 +8,8 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-namespace Dinkbit\SkelPack\Tests;
+
+namespace Dinkbit\SkelPack\tests;
 
 /**
  * This is the skelpack class.
